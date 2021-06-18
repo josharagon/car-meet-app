9def8f1fc703ee1db317c45f0473672d681410b2
Josh Aragon <josharagon99@gmail.com>
2021-06-18 12:19:10 -0600

    Install router:

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { NativeRouter, Switch, Route } from 'react-router-native';
 
 export default function App() {
 
   const [user, setUser] = useState({id: 69, name: 'Josh', car: {year: 2018, make: 'Ford', model: 'Fiesta', trim: 'ST', mods:{intake: {brand: 'injen', model: 'short ram', linK: null}, intercooler: {brand: 'whoosh', model: 'v1', linK: null}, turbo: {brand: 'garrett', model: '2860r gen ii', linK: null} } }, memberSince: new Date() })
     
   return (
+    <NativeRouter>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Button title={"Change Text"}/>
     </View>
+    </NativeRouter>
   );
 }
 
