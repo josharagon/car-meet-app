1671ea964752c3ed67482ff768f546a263c3374e
Josh Aragon <josharagon99@gmail.com>
2021-06-19 13:42:54 -0600

    Add map

--- App.js
+++ App.js
@@ -1,35 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NativeRouter, Switch, Route } from 'react-router-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NativeRouter, Switch, Route } from "react-router-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import MainMap from './components/MainMap.js'
-import UserSearch from './components/UserSearch'
-import UserProfile from './components/UserProfile'
-
+import MainMap from "./components/MainMap.js";
+import UserSearch from "./components/UserSearch";
+import UserProfile from "./components/UserProfile";
 
 export default function App() {
+  const [user, setUser] = useState({
+    id: 69,
+    name: "Josh",
+    car: {
+      year: 2018,
+      make: "Ford",
+      model: "Fiesta",
+      trim: "ST",
+      mods: {
+        intake: { brand: "injen", model: "short ram", linK: null },
+        intercooler: { brand: "whoosh", model: "v1", linK: null },
+        turbo: { brand: "garrett", model: "2860r gen ii", linK: null },
+      },
+    },
+    memberSince: new Date(),
+  });
 
-  const [user, setUser] = useState({id: 69, name: 'Josh', car: {year: 2018, make: 'Ford', model: 'Fiesta', trim: 'ST', mods:{intake: {brand: 'injen', model: 'short ram', linK: null}, intercooler: {brand: 'whoosh', model: 'v1', linK: null}, turbo: {brand: 'garrett', model: '2860r gen ii', linK: null} } }, memberSince: new Date() })
-    
+  const Tab = createBottomTabNavigator();
+
   return (
     <NativeRouter>
-    <View style={styles.container}>
-      <Switch>
-        <Route exact path="home" component={MainMap}/>
-        <Route exact path="search" component={UserSearch}/>
-        <Route exact path="profile" component={UserProfile}/>
-      </Switch>
-    </View>
+      <View style={styles.container}>
+        <Switch>
+          <Route exact path="/" component={MainMap} />
+          <Route exact path="search" component={UserSearch} />
+          <Route exact path="profile" component={UserProfile} />
+        </Switch>
+      </View>
     </NativeRouter>
+    // <NavigationContainer>
+    //   <Tab.Navigator>
+    //     <Tab.Screen name="Home" component={MainMap} />
+    //     <Tab.Screen name="Search" component={UserSearch} />
+    //     <Tab.Screen name="Profile" component={UserProfile} />
+    //   </Tab.Navigator>
+    // </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/MainMap.js
+++ components/MainMap.js
@@ -1,1 +1,47 @@
-// import MapView from 'react-native-maps';
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView, { MapKit, Marker, Callout } from "react-native-maps";
+
+const MainMap = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={MapKit}
+        style={styles.map}
+        // showsUserLocation={true}
+        // followsUserLocation={true}
+        region={{
+          latitude: 39.973851,
+          longitude: -104.86436,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 39.973851,
+            longitude: -104.86436,
+          }}
+          image={require("../assets/map-marker.png")}
+          title="Test Location"
+          description="test desc"
+        ></Marker>
+      </MapView>
+    </View>
+  );
+};
+
+export default MainMap;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
